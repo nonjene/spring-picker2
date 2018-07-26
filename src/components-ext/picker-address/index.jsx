848781cd3ffe99e259831c222e227476030decc6
nonjene <reanfn89@gmail.com>
2018-07-26 15:49:33 +0800

    addr demo

--- src/components-ext/picker-address/index.jsx
+++ src/components-ext/picker-address/index.jsx
@@ -1,117 +1,145 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Picker, Popup } from '../../components';
+import { PopupPicker } from '../../components';
 import { provins, citys, areas } from './address';
 import './index.scss';
 
+const getProvinsList = () => {
+  return provins.map((name, i) => ({ name, value: i }));
+};
+
+const getCitysList = provinName => {
+  return citys[provinName].map((name, i) => ({ name, value: i }));
+};
+
+const getAreasList = citysName => {
+  return areas[citysName].map((name, i) => ({ name, value: i }));
+};
+
+const getProvNameByIndex = ind => {
+  return provins[ind];
+};
+
+const getCityNameByIndex = (provInd, cityInd) => {
+  return citys[getProvNameByIndex(provInd)][cityInd];
+};
+
+const getAreaNameByIndex = (provInd, cityInd, areaInd) => {
+  return areas[getCityNameByIndex(provInd, cityInd)][areaInd];
+};
+
 const propTypes = {
-  defaultValue: PropTypes.array.isRequired,
-  onConfirm: PropTypes.func.isRequired,
-  onCancel: PropTypes.func.isRequired,
-  onChange: PropTypes.func.isRequired,
-  visible: PropTypes.bool.isRequired,
-}
+  selectedValue: PropTypes.object,
+  onCancel: PropTypes.func,
+  onSelect: PropTypes.func.isRequired,
+  onChanging: PropTypes.func
+};
+const defaultProps = {
+  visible: false,
+  selectedValue: null
+};
 
 class PickerAddress extends React.Component {
   constructor(props) {
-    super();
+    super(props);
     this.props = props;
-    this.address = [];
+    const selectedValue = props.selectedValue
+      ? props.selectedValue
+      : {
+          prov: 0,
+          city: 0,
+          area: 0
+        };
+    this.state = {
+      visible: props.visible,
+      selectedValue
+    };
   }
 
-  initDefaultData () {
-    this.data = {
-      provins: {
-        list: provins,
-        defaultValue: this.props.defaultValue[0],
-        displayValue (name) {
-          return name;
-        }
+  handleSelect(selectedValue, selectedName) {
+    this.setState({
+      visible: false
+    });
+    this.props.onSelect(selectedValue, selectedName);
+  }
+
+  handleChanging(selectedValue, key, value, name) {
+    console.log(selectedValue, key, value, name);
+    let { prov, city, area } = selectedValue;
+    switch (key) {
+      case 'prov':
+        city = 0;
+        area = 0;
+        break;
+      case 'city':
+        area = 0;
+        break;
+    }
+    this.setState(
+      {
+        selectedValue: { prov, city, area }
       },
-      citys: {
-        list: citys[this.props.defaultValue[0]],
-        defaultValue: this.props.defaultValue[1],
-        displayValue (name) {
-          return name;
-        }
-      },
-      areas: {
-        list: areas[this.props.defaultValue[1]],
-        defaultValue: this.props.defaultValue[2],
-        displayValue (name) {
-          return name;
-        }
+      () => {
+        this.props.onChanging &&
+          this.props.onChanging(selectedValue, key, value, name);
       }
+    );
+  }
+
+  handleCancel() {
+    this.setState({ visible: false }, () => {
+      this.props.onCancel && this.props.onCancel();
+    });
+  }
+  componentWillReceiveProps(nexProps) {
+    if (
+      this.props.visible !== nexProps.visible &&
+      nexProps.visible !== this.state.visible
+    ) {
+      this.setState({
+        visible: nexProps.visible
+      });
+    }
+
+    // 选项更新
+    if (
+      nexProps.selectedValue &&
+      Object.keys(nexProps.selectedValue).find(
+        key => this.state.selectedValue[key] !== nexProps.selectedValue[key]
+      )
+    ) {
+      this.setState({
+        selectedValue: nexProps.selectedValue
+      });
     }
   }
 
-  handleChangeProvin (provin) {
-    this.data.provins = {
-      list: provins,
-      defaultValue: provin,
-    };
-    this.data.citys = {
-      list: citys[provin],
-      defaultValue: citys[provin][0],
-    };
-    this.data.areas = {
-      list: areas[citys[provin][0]],
-      defaultValue: areas[citys[provin][0]][0],
-    },
-    this.address = [];
-    this.address.push(provin);
-    this.address.push(citys[provin][0]);
-    this.address.push(areas[citys[provin][0]][0]);
-    this.props.onChange(this.address);
-  }
-
-  handleChangeCity(city) {
-    this.address[1] = city;
-    this.address[2] = areas[city][0];
-    this.data.areas = {
-      list: areas[city],
-      defaultValue: areas[city][0],
-    };
-    this.props.onChange(this.address);
-  }
-
-  handleChangeArea(area) {
-    this.address[2] = area;
-    this.props.onChange(this.address);
-  }
-
-  handleClose () {
-    this.props.onConfirm(this.address)
-  }
-
-  handleCancel () {
-    this.props.onCancel()
-  }
-
-  render () {
-    this.initDefaultData();
-    return <div className="ui-picker-address">
-      <Popup
-        onConfirm={this.handleClose.bind(this)}
-        onCancel={this.handleCancel.bind(this)}
-        visible={this.props.visible}>
-        <Picker
-          onChange={this.handleChangeProvin.bind(this)}
-          data={this.data.provins}
+  render() {
+    const selected = this.state.selectedValue;
+    const provList = getProvinsList();
+    const cityList = getCitysList(getProvNameByIndex(selected.prov || 0));
+    const areaList = getAreasList(
+      getCityNameByIndex(selected.prov || 0, selected.city || 0)
+    );
+    return (
+      <div className="ui-picker-address">
+        <PopupPicker
+          data={{
+            prov: provList,
+            city: cityList,
+            area: areaList
+          }}
+          selectedValue={selected}
+          visible={this.state.visible}
+          onCancel={this.handleCancel.bind(this)}
+          onSelect={this.handleSelect.bind(this)}
+          onChanging={this.handleChanging.bind(this)}
         />
-        <Picker
-          onChange={this.handleChangeCity.bind(this)}
-          data={this.data.citys}
-        />
-        <Picker
-          onChange={this.handleChangeArea.bind(this)}
-          data={this.data.areas}
-        />
-      </Popup>
-    </div>
+      </div>
+    );
   }
 }
 
 PickerAddress.propTypes = propTypes;
-
+PickerAddress.defaultProps = defaultProps;
 export default PickerAddress;
